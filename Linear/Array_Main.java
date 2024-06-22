8ebd4e776f34e833c46acae2effb4424e372244b
sooshiance <s.katafract@gmail.com>
2024-06-22 19:11:13 +0330

    add search function in arrays

--- Linear/Array_Main.java
+++ Linear/Array_Main.java
@@ -2,19 +2,26 @@ public class Array_Main
 {
     static void showArray(int arr[])
     {
-        for (int i = 0; i < arr.length; i++)
-        {
+        for (int i = 0; i < arr.length; i++) 
             System.out.println(arr[i]);
-        }
-    }   
+    }
+    
+    static int findElement(int arr[], int n, int key)
+    {
+        for (int i = 0; i < n; i++)
+            if (arr[i] == key)
+                return i;
+
+        return -1;
+    }
 
     public static void main(String args[])
     {
         int sorted_array[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
-
-        showArray(sorted_array);
-
+        
         int unsorted_array[] = { 6, 14, 23, -10, 5, 0, -19, 25 };
+        
+        showArray(sorted_array);
 
         showArray(unsorted_array);
     }
