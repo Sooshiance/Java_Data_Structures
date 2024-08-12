afacaac6fdda194164fe0d5890ddd6084cb1d5cc
sooshiance <s.katafract@gmail.com>
2024-08-12 22:41:34 +0330

    build: Create basic module for Array

---
+++ Linear/Array/Main.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        // Defining Arrays : 
+        // sorted
+        int sort = new int [0,1,2,3,4,5,6,7,8,9];
+        // unsorted
+        int unsort = new int [12,6,41,-2,50,0,24,15,9,35];
+    }
+}
