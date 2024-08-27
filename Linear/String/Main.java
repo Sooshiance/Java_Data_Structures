4f97bdb715d0fbe273f84083ef6376b9030ee4b9
sooshiance <s.katafract@gmail.com>
2024-08-27 07:58:11 +0330

    build: Create string classes

---
+++ Linear/String/Main.java
@@ -0,0 +1,12 @@
+package String;
+
+class Main
+{
+    public static void main(String args[])
+    {
+        String string1 = new String("Mehdi Davari");
+        String string2 = new String("Programmer");
+        String string3 = new String("Mehdi");
+        String string4 = new String("Mehdi");
+    }
+}
---
+++ Linear/String/Operations.java
@@ -0,0 +1,6 @@
+package String;
+
+class Operation
+{
+    
+}
