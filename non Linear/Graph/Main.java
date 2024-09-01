ec0327266ed6ae86c426ee2fd90e6a53972b71c6
sooshiance <s.katafract@gmail.com>
2024-09-01 13:34:43 +0330

    build: Graph data structure added

---
+++ non Linear/Graph/Main.java
@@ -0,0 +1,59 @@
+package Graph;
+
+import java.util.*;
+
+public class Main
+{
+    private int V;
+    private LinkedList<Integer> adj[];
+
+    Main(int v)
+    {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList();
+    }
+
+    void addEdge(int v, int w)
+    {
+        adj[v].add(w);
+    }
+
+    void DFSUtil(int v, boolean visited[])
+    {
+        visited[v] = true;
+        System.out.print(v + " ");
+
+        Iterator<Integer> it = adj[v].listIterator();
+        while (it.hasNext())
+        {
+            int n = it.next();
+            if (!visited[n])
+                DFSUtil(n, visited);
+        }
+    }
+
+    void DFS(int v)
+    {
+        boolean visited[] = new boolean[V];
+        DFSUtil(v, visited);
+        System.out.println();
+    }
+
+    public static void main(String args[])
+    {
+        Main g = new Main(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        System.out.println("Depth First Traversal starting from vertex 2:");
+        
+        g.DFS(2);
+    }
+}
